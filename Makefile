165f9f71223880eb96fb19455f64c32576aa7df7
Mason Pawsey <mpawsey@odin.cs.csubak.edu>
2019-02-14 11:34:34 -0800

    move into new directory

---
+++ Makefile
@@ -0,0 +1,12 @@
+CFLAGS = -I ./include
+LFLAGS = -lrt -lX11 -lGLU -lGL -pthread -lm #-lXrandr
+
+all: walk2
+
+walk2: walk2.cpp log.cpp masonP.cpp hasunK.cpp fernandoH.cpp
+	g++ $(CFLAGS) walk2.cpp log.cpp masonP.cpp hasunK.cpp fernandoH.cpp libggfonts.a -Wall -Wextra $(LFLAGS) -owalk2
+
+clean:
+	rm -f walk2
+	rm -f *.o
+
---
+++ fernandoH.cpp
@@ -0,0 +1,36 @@
+// Program: fernandoH.cpp
+// CMPS3350
+// Author: Fernando
+
+
+
+#include <iostream>
+#include <GL/glx.h>
+#include "fonts.h"
+#include <math.h>
+
+
+void showFernandoPicture(int x, int y, GLuint textid) 
+{
+	glColor3ub(255,255,255);
+	static float angle = 0.0f;
+	static int wid = 30;
+	wid += sin(angle) * 10;
+	float fx = (float)x;
+	float fy = (float)y;
+	float a = 0;
+	a += sin(angle) * 10.0f;
+	fx += a;
+	angle += 0.2f;
+	glPushMatrix();
+	glTranslatef(fx,fy,0);
+	glRotatef(a, 0, 0, 1.0);
+	glBindTexture(GL_TEXTURE_2D, textid);
+	glBegin(GL_QUADS);
+		glTexCoord2f(0.0f, 1.0f); glVertex21(-wid,-wid);
+		glTexCoord2f(0.0f, 0.0f); glVertex21(-wid,-wid);
+		glTexCoord2f(1.0f, 1.0f); glVertex21(-wid,-wid);
+		glTexCoord2f(1.0f, 1.0f); glVertex21(-wid,-wid);
+	glEnd();
+	glPopMatrix();
+}
