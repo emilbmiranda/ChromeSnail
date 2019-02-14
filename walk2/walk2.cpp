02db12f8f527cc3cb6175b5f30ef95a53e9952b6
Mason Pawsey <mpawsey@odin.cs.csubak.edu>
2019-02-14 11:10:47 -0800

    update credits instructions on main screen

--- walk2/walk2.cpp
+++ walk2/walk2.cpp
@@ -953,6 +953,7 @@ void render(void)
 		ggprint8b(&r, 16, c, "right arrow -> walk right");
 		ggprint8b(&r, 16, c, "left arrow  <- walk left");
 		ggprint8b(&r, 16, c, "frame: %i", gl.walkFrame);
+		ggprint8b(&r, 16, c, "credits   c");
 		if (gl.movie) {
 			screenCapture();
 		}
