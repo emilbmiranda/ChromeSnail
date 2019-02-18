69c2bbd8b665bf333028393d0fa61aba16bc8c2e
Fernando Herrera <fherrera2018@gmail.com>
2019-02-17 19:53:35 -0800

    Image is static. Non of that seizure business.

--- fernandoH.cpp
+++ fernandoH.cpp
@@ -15,12 +15,12 @@ void showFernandoPicture(int x, int y, GLuint textid)
 	glColor3ub(255,255,255);
 	static float angle = 0.0f;
 	static int wid = 30;
-	wid += sin(angle) * 10;
+	//wid += sin(angle) * 10;
 	float fx = (float)x;
 	float fy = (float)y;
-	float a = 0;
-	a += sin(angle) * 10.0f;
-	fx += a;
+	//float a = 0;
+	//a += sin(angle) * 10.0f;
+	//fx += a;
 	angle += 0.2f;
 	glPushMatrix();
 	glTranslatef(fx,fy,0);
