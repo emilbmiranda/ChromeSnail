f3a2276d9522178b2433f8c2f106e0c85ec2bfbe
Mason Pawsey <mpawsey@odin.cs.csubak.edu>
2019-02-18 10:53:09 -0800

    update masonp.cpp

--- masonP.cpp
+++ masonP.cpp
@@ -18,17 +18,17 @@ void showMasonPicture(int x, int y, GLuint textid)
 {
 	glColor3ub(255,255,255);
 	static float angle = 0.0f;
-	static int wid = 30;
-	//wid += sin(angle) * 10;
+	static int wid = 50;
+	wid += sin(angle) * 2;
 	float fx = (float)x;
 	float fy = (float)y;
-	//float a = 0;
-	//a += sin(angle) * 10.0f;
-	//fx += a;
-	//angle += 0.2f;
+	float a = 0;
+	a += sin(angle) * 5.0f;
+	fx += a;
+	angle += 0.2f;
 	glPushMatrix();
 	glTranslatef(fx,fy,0);
-	//glRotatef(a, 0, 0, 1.0);
+	glRotatef(a, 0, 0, 1.0);
 	glBindTexture(GL_TEXTURE_2D, textid);
 
 	glBegin(GL_QUADS);
