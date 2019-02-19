67073608c26a65c7b14d70aa85fd415c57c39ad2
Fernando Herrera <fherrera2018@gmail.com>
2019-02-18 17:11:00 -0800

    Cleaned up my file some

--- fernandoH.cpp
+++ fernandoH.cpp
@@ -1,9 +1,6 @@
 // Program: fernandoH.cpp
 // CMPS3350
 // Author: Fernando
-
-
-
 #include <iostream>
 #include <GL/glx.h>
 #include "fonts.h"
@@ -13,18 +10,11 @@
 void showFernandoPicture(int x, int y, GLuint textid) 
 {
 	glColor3ub(255,255,255);
-	static float angle = 0.0f;
 	static int wid = 30;
-	//wid += sin(angle) * 10;
 	float fx = (float)x;
 	float fy = (float)y;
-	//float a = 0;
-	//a += sin(angle) * 10.0f;
-	//fx += a;
-	angle += 0.2f;
 	glPushMatrix();
 	glTranslatef(fx,fy,0);
-	//glRotatef(a, 0, 0, 1.0);
 	glBindTexture(GL_TEXTURE_2D, textid);
 	glBegin(GL_QUADS);
 		glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
