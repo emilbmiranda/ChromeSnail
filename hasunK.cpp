ef4da2e693a3fa2455063b6cbbbaecd6977068d2
hasun <hkhan4@csub.edu>
2019-02-20 11:20:48 -0800

    Fixed hasunK.cpp K&R formatting

--- hasunK.cpp
+++ hasunK.cpp
@@ -3,13 +3,11 @@
 //author:  Hasun Khan 
 //date:    Spring 2019
 //
-// Display my picture and write my name as a part of the credits 
+//Display my picture and write my name as a part of the credits 
 #include <iostream>
 #include <GL/glx.h>
 #include "fonts.h"
 #include <math.h>
-
-
 
 //Prints my name to the Screen
 void printHasunName(Rect x, int y)
@@ -17,18 +15,17 @@ void printHasunName(Rect x, int y)
 	ggprint8b(&x, y, 0x00ff5900, "Hasun Khan");
 }
 
-
 //Displays an image to the screen
 void showHasunPicture(int x, int y, GLuint textid)
-{ 
+{
 	glColor3ub(255,255,255);
 	static float angle = 0.0f;
 	static int w = 30; 
 	w +=sin(angle)*10;
 	float fx = (float)x;
 	float fy = (float)y;
-	float a = 0; 
-	a += sin(angle)*10.0f;
+	float a = 1; 
+	//a += sin(angle)*10.0f;
 	//fx += a;
 	angle += 0.2f;
 	glPushMatrix(); 
@@ -42,7 +39,4 @@ void showHasunPicture(int x, int y, GLuint textid)
 		glTexCoord2f(1.0f,1.0f); glVertex2i(w,-w);
 	glEnd();
 	glPopMatrix();
-} 
-
-
-
+}
