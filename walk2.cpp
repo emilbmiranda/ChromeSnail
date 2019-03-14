6ffd498ba9081268f332357c53042cb6b6579c28
Emil Miranda <emilbmiranda@gmail.com>
2019-03-14 11:51:48 -0700

    fixed what was merged in walk2.cpp

--- walk2.cpp
+++ walk2.cpp
@@ -525,24 +525,11 @@ void checkMouse(XEvent *e)
 	}
 	if (e->type == ButtonPress) {
 		if (e->xbutton.button==1) {
-<<<<<<< HEAD
 			cout << "Mouse press" << endl;
 			extern Rect create_menu_button(int gl_xres, int gl_yres);
 			Rect menu = create_menu_button(gl.xres, gl.yres);
 			extern void check_menu_press(XEvent *e, Rect r, int *global);
 			check_menu_press(e, menu, &gl.showCredits);
-=======
-			//Left button is down
-			cout << "left mouse button press" << endl;
-			struct timespec bt;
-			clock_gettime(CLOCK_REALTIME, &bt);
-			double ts = timers.timeDiff(&bullets.bulletTimer, &bt);
-			if (ts > 0.1) {
-				cout << "about to shoot" << endl;
-				timers.timeCopy(&bullets.bulletTimer, &bt);
-				shootBullet(&bullets, &bt);
-			}
->>>>>>> 432253eb73d8cda48c8a11f062a3681b762838a9
 		}
 		if (e->xbutton.button==3) {
 			//Right button is down
@@ -1032,7 +1019,6 @@ void render(void)
 		}
 		
 		//will add to menu
-<<<<<<< HEAD
 		 unsigned int c = 0x00ffff44;
 		 r.bot = gl.yres - 20;
 		 r.left = 10;
@@ -1048,25 +1034,5 @@ void render(void)
 		if (gl.movie) {
 			screenCapture();
 		}
-=======
-		// unsigned int c = 0x00ffff44;
-		// r.bot = gl.yres - 20;
-		// r.left = 10;
-		// r.center = 0;
-		// ggprint8b(&r, 16, c, "W   Walk cycle");
-		// ggprint8b(&r, 16, c, "E   Explosion");
-		// ggprint8b(&r, 16, c, "+   faster");
-		// ggprint8b(&r, 16, c, "-   slower");
-		// ggprint8b(&r, 16, c, "right arrow -> walk right");
-		// ggprint8b(&r, 16, c, "left arrow  <- walk left");
-		// ggprint8b(&r, 16, c, "frame: %i", gl.walkFrame);
-		// ggprint8b(&r, 16, c, "credits   c");
-		// if (gl.movie) {
-		// 	screenCapture();
-		// }
-
-		//draw bullets
-		drawBullets(&bullets);
->>>>>>> 432253eb73d8cda48c8a11f062a3681b762838a9
 	}
 }
