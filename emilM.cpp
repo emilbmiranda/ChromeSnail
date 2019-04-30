63750188765aa56f3a242a28745a16bbe9bf83fc
Mason Pawsey <mpawsey@odin.cs.csubak.edu>
2019-04-30 08:24:20 -0700

    formatting

--- emilM.cpp
+++ emilM.cpp
@@ -9,102 +9,118 @@
 
 void showEmil(Rect r, int y)
 {
-    ggprint8b(&r, y, 0xffff66, "Emil Miranda");
+	ggprint8b(&r, y, 0xffff66, "Emil Miranda");
 }
 
 void showEmilPicture(int x, int y, GLuint textid)
 {
-    glColor3ub(255,255,255);
-    static float angle = 0.0f;
-    static int wid = 30;
-    wid += sin(angle) * 10;
-    float fx = (float)x;
-    float fy = (float)y;
-    float a = 0;
-    a += sin(angle) * 10.0f;
-    fx += a;
-    angle += 0.2f;
-    glPushMatrix();
-    glTranslatef(fx,fy,0);
-    glRotatef(a, 0, 0, 1.0);
-    glBindTexture(GL_TEXTURE_2D, textid);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
-    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
-    glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
-    glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
-    glEnd();
-    glPopMatrix();
+	glColor3ub(255,255,255);
+	static float angle = 0.0f;
+	static int wid = 30;
+	wid += sin(angle) * 10;
+	float fx = (float)x;
+	float fy = (float)y;
+	float a = 0;
+	a += sin(angle) * 10.0f;
+	fx += a;
+	angle += 0.2f;
+	glPushMatrix();
+	glTranslatef(fx,fy,0);
+	glRotatef(a, 0, 0, 1.0);
+	glBindTexture(GL_TEXTURE_2D, textid);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex2i(-wid,-wid);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex2i(-wid, wid);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex2i( wid, wid);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex2i( wid,-wid);
+	glEnd();
+	glPopMatrix();
 }
 
 void create_menu_button(int gl_xres, int gl_yres)
 {
-    Rect r;
-    r.bot = gl_yres - 20;
-    r.left = gl_xres - 50;
-    r.center = 0;
-    r.height = 200;
-    r.width = 100;
-    ggprint12(&r, 100, 0xffffff, "Menu");
+	Rect r;
+	r.bot = gl_yres - 20;
+	r.left = gl_xres - 50;
+	r.center = 0;
+	r.height = 200;
+	r.width = 100;
+	ggprint12(&r, 100, 0xffffff, "Menu");
 }
 
 void start_menu(int xres, int yres, GLuint startMenuTexture)
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    static int wid = xres/2;
-    float fx = (float)xres;
-    float fy = (float)yres;
-    glPushMatrix();
-    glTranslatef(fx/2,fy/2,0);
-    glBindTexture(GL_TEXTURE_2D, startMenuTexture);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
-    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
-    glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
-    glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
-    glEnd();
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glPopMatrix();
+	glClear(GL_COLOR_BUFFER_BIT);
+	static int wid = xres/2;
+	float fx = (float)xres;
+	float fy = (float)yres;
+	glPushMatrix();
+	glTranslatef(fx/2,fy/2,0);
+	glBindTexture(GL_TEXTURE_2D, startMenuTexture);
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex2i(-wid,-wid);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex2i(-wid, wid);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex2i( wid, wid);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex2i( wid,-wid);
+	glEnd();
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glPopMatrix();
 }
 
 void show_logo(int xres, int yres, GLuint logoTexture)
 {
-    static int wid = xres/4.5;
-    float fx = (float)xres;
-    float fy = (float)yres+300;
-    glPushMatrix();
-    glTranslatef(fx/2,fy/2,0);
-    glBindTexture(GL_TEXTURE_2D, logoTexture);
-    glEnable(GL_ALPHA_TEST);
+	static int wid = xres/4.5;
+	float fx = (float)xres;
+	float fy = (float)yres+300;
+	glPushMatrix();
+	glTranslatef(fx/2,fy/2,0);
+	glBindTexture(GL_TEXTURE_2D, logoTexture);
+	glEnable(GL_ALPHA_TEST);
 	glAlphaFunc(GL_GREATER, 0.0f);
 	glColor4ub(255,255,255,255);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
-    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
-    glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
-    glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
-    glEnd();
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glPopMatrix();
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex2i(-wid,-wid);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex2i(-wid, wid);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex2i( wid, wid);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex2i( wid,-wid);
+	glEnd();
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glPopMatrix();
 }
 
 void show_keys(int xres, int yres, GLuint keysTexture)
 {
-    static int wid = xres/5;
-    float fx = (float)xres;
-    float fy = (float)yres-300;
-    glPushMatrix();
-    glTranslatef(fx/2,fy/2,0);
-    glBindTexture(GL_TEXTURE_2D, keysTexture);
-    glEnable(GL_ALPHA_TEST);
+	static int wid = xres/5;
+	float fx = (float)xres;
+	float fy = (float)yres-300;
+	glPushMatrix();
+	glTranslatef(fx/2,fy/2,0);
+	glBindTexture(GL_TEXTURE_2D, keysTexture);
+	glEnable(GL_ALPHA_TEST);
 	glAlphaFunc(GL_GREATER, 0.0f);
 	glColor4ub(255,255,255,255);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 1.0f); glVertex2i(-wid,-wid);
-    glTexCoord2f(0.0f, 0.0f); glVertex2i(-wid, wid);
-    glTexCoord2f(1.0f, 0.0f); glVertex2i( wid, wid);
-    glTexCoord2f(1.0f, 1.0f); glVertex2i( wid,-wid);
-    glEnd();
-    glBindTexture(GL_TEXTURE_2D, 0);
-    glPopMatrix();
+	glBegin(GL_QUADS);
+	glTexCoord2f(0.0f, 1.0f);
+	glVertex2i(-wid,-wid);
+	glTexCoord2f(0.0f, 0.0f);
+	glVertex2i(-wid, wid);
+	glTexCoord2f(1.0f, 0.0f);
+	glVertex2i( wid, wid);
+	glTexCoord2f(1.0f, 1.0f);
+	glVertex2i( wid,-wid);
+	glEnd();
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glPopMatrix();
 }
